7cd5694ebab66c8887b0f4e5b138271945043ee1
lorenzospadotto-debug <lorenzo.spadotto@gmail.com>
2025-08-29 19:17:00 +0200

    Update app.js

--- static/js/app.js
+++ static/js/app.js
@@ -7,3 +7,23 @@ function copyText(el){
   try { document.execCommand('copy'); } catch (e) {}
   sel.removeAllRanges();
 }
+
+function dragDrop(){
+  return {
+    over:false,
+    handleDrop(e){
+      this.over=false;
+      const dt = e.dataTransfer;
+      if(dt && dt.files && dt.files.length){
+        const form = document.getElementById('uploadForm');
+        const input = form.querySelector('input[name="file"]');
+        input.files = dt.files;
+        form.submit();
+      }
+    },
+    submitOnChange(e){
+      const form = document.getElementById('uploadForm');
+      form.submit();
+    }
+  }
+}
