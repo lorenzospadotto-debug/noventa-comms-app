fb795f8ab78034732f5cda206a933773228afc69
lorenzospadotto-debug <lorenzo.spadotto@gmail.com>
2025-08-29 23:57:57 +0200

    Update app.js

--- static/js/app.js
+++ static/js/app.js
@@ -12,20 +12,18 @@ function dragDrop(){
   return {
     over:false,
     startLoading(ev){
-      // usa l'overlay definito in base.html (x-data="{}" su <body>)
       const root = document.querySelector('body');
-      if(root && root.__x) { root.__x.$data.loading = true; }
+      if(root && root.__x){ root.__x.$data.loading = true; }
     },
     handleDrop(e){
       this.over=false;
       const dt = e.dataTransfer;
       if(dt && dt.files && dt.files.length){
         const form = document.getElementById('composeForm');
-        const input = form.querySelector('input[name="file"]');
+        const input = form.querySelector('input[name="files"]'); // MULTIPLO
         input.files = dt.files;
-        // mostra spinner prima di inviare
         const root = document.querySelector('body');
-        if(root && root.__x) { root.__x.$data.loading = true; }
+        if(root && root.__x){ root.__x.$data.loading = true; }
         form.submit();
       }
     }
